32f38323e9be5f9e72e1a1ab2aab1e850ec1c5f3
Dobrodeev <dobrod@meta.ua>
2018-03-08 14:22:18 +0200

    применили функции natsort() & usort()

--- arrays_sort.php
+++ arrays_sort.php
@@ -28,16 +28,117 @@ for ($i = 0; $i < count($number); $i++)
 $nabor_slov = ['one', 'two', 'three', 'abs', 'four', 'five', 'apocalyptica', 'tree.js', 'dimension', 'rot', 'div', 'grad',
     'bessel function', 'white noize', 'neman function', 'laplas', 'hevisaid', 'dimension', 'nonlinear systems',
     'nonlinear stohastical dynamics', 'ricci flows'];
+#
 function array_writeln($arr)
 {
     echo '<pre>';
     print_r($arr);
     echo '</pre>';
 }
+#
 array_writeln($nabor_slov);
 sort($nabor_slov);
 echo 'after sort()<br>';
 array_writeln($nabor_slov);
+#
+function array_write($arr)
+{
+    for ($i = 0; $i < count($arr); $i++)
+    {
+        echo $arr[$i].' ';
+    }
+    echo '<br>';
+}
+#
+$number = [2 ,1, 4, 3, 5];
+echo 'до сортировки: <br>';
+array_write($number);
+rsort($number);
+echo '<br> после сортировки: <br>';
+array_write($number);
+$array = ['a'=>'one',
+        'b'=>'two',
+        'c'=>'three',
+        'd'=>'five'
+        ];
+array_write_assoc($array);
+asort($array);
+#
+function array_write_assoc($arr)
+{
+    foreach ($arr as $key=>$value)
+    {
+        echo "$key=>$value".' ';
+    }
+    echo '<br>';
+}
+#
+array_write_assoc($array);
+arsort($array);
+array_write_assoc($array);
+$arr = ['a'=>'one', 'd'=>'four', 'c'=>'three', 'b'=>'two'];
+array_write_assoc($arr);
+echo 'array after ksort():<br>';
+ksort($arr);
+array_write_assoc($arr);
+echo 'array after krsort():<br>';
+krsort($arr);
+array_write_assoc($arr);
+$arr_first = $arr_second = ['file12.txt', 'file10.txt', 'file2.txt', 'file1.txt'];
+echo 'Массив для сортировки: <br>';
+array_writeln($arr_second);
+sort($arr_first);
+echo 'Обычная сортировка: <br>';
+array_writeln($arr_first);
+natsort($arr_second);
+echo 'Естественная сортировка: <br>';
+array_writeln($arr_second);
+$arr1 = [3, 4, 2, 7, 1, 5];
+$arr2 = ['world', 'Hello', 'yes', 'no', 'apple', 'wet'];
+array_writeln($arr1);
+array_writeln($arr2);
+echo 'After array_multisort();<br>';
+array_multisort($arr1, $arr2);
+array_writeln($arr1);
+array_writeln($arr2);
+echo 'Модифицированный вариант сортировки в обратном порядке: <br>';
+$arr1 = [3, 4, 2, 7, 1, 5];
+$arr2 = ['world', 'Hello', 'yes', 'no', 'apple', 'wet'];
+array_multisort($arr1, SORT_DESC, SORT_NUMERIC, $arr2);
+array_writeln($arr1);
+array_writeln($arr2);
+# Пример #1 Пример использования usort()
+echo 'Пример #1 Пример использования usort():<br>';
+function cmp($a, $b)
+{
+    if ($a == $b) {
+        return 0;
+    }
+    return ($a < $b) ? -1 : 1;
+}
+
+$a = array(3, 2, 5, 6, 1);
+
+usort($a, "cmp");
+
+foreach ($a as $key => $value) {
+    echo "$key: $value\n";
+}
+echo '<br>Пример #2 Пример использования функции usort() с многомерными массивами <br>';
+function cmp2($a, $b)
+{
+    return strcmp($a["fruit"], $b["fruit"]);
+}
+
+$fruits[0]["fruit"] = "lemons";
+$fruits[1]["fruit"] = "apples";
+$fruits[2]["fruit"] = "grapes";
+
+usort($fruits, "cmp2");
+
+while (list($key, $value) = each($fruits)) {
+    echo "\$fruits[$key]: " . $value["fruit"] . "\n";
+}
 ?>
 </body>
 </html>
