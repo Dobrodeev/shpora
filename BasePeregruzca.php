97f20f1bf7a0db9c1bf364cd523577df3582d004
Dobrodeev <dobrod@meta.ua>
2018-03-13 00:09:34 +0200

    пример перегрузки: метод производного класса заменяет метод базового

---
+++ BasePeregruzca.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Я
+ * Date: 13.03.2018
+ * Time: 0:01
+ */
+
+class BasePeregruzca
+{
+    public function printVar()
+    {
+        echo 'Вызов метода printVar() базового класса <br>';
+    }
+}
