bc7e22f3da4239c7842e9fb50355d0a88e22abf7
Dobrodeev <dobrod@meta.ua>
2018-03-25 20:18:12 +0300

    ïîäêëþ÷èëè Bootstrap

--- masiv.php
+++ masiv.php
@@ -6,8 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Lection 2</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 </head>
 <body>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
 <?php
 /**
  * Created by PhpStorm.
@@ -18,7 +21,15 @@
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 echo ROOT.'<br>';
 $masiv = array('M', 'K', 'Git', 27);
+#
+echo '<pre>';
 var_dump($masiv);
+echo '</pre>';
+#
+echo '<pre>';
+print_r($masiv);
+echo '</pre>';
+
 $x = '25.3';
 floatval($x);
 settype($x, int);
