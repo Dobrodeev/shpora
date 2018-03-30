aa9b867d7e3908381888170cf474ef997915ac93
Dobrodeev <dobrod@meta.ua>
2018-03-30 19:02:40 +0300

    создали массив учебных заведений

--- masiv2.php
+++ masiv2.php
@@ -3,9 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>for Consts</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 </head>
 <body>
-	<?php
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<?php
 define('WHAT_MAKE', 'Fucking my heart');
 define('WHERE_CITY', 'Kiev');
 define('WHEN_BACHELOUR', 'in 2014');
@@ -13,7 +16,30 @@ define('WHEN_EXPERT', 'in 2018');
 
 $universityFirst = 'KPI';
 $universitySecond = 'NAU';
+$universityThird = 'KNU';
+$universityFourth = 'Dragomanova';
+$universityFifth = 'Ukraine';
+$universitySix = 'Technicum';
+$universityArray = [$universityFirst, $universitySecond, $universityThird, $universityFourth, $universityFirst, $universitySix];
+$universityAccredetation = [4, 4, 4, 4, 4, 3];
+$universityAll = array_combine($universityArray, $universityAccredetation);
 
+function consoleWriteline($arr) {
+	echo '<pre>';
+	print_r($arr);
+	echo '</pre>';
+}
+
+function consoleWritelineVar($arr) {
+	echo '<pre>';
+	var_dump($arr);
+	echo '</pre>';
+}
+
+// $universityAll['Училище'] = 'Медицинское';
+$universityAll['Училище'] = ['Медицинское', 'Энергетическое', 'Механическое'];
+consoleWriteline($universityAll);
+consoleWritelineVar($universityAll);
 ?>
 </body>
 </html>
