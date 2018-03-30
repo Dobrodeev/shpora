ed6d447a211f1261f12c2018e43f0bacbfd81449
Dobrodeev <dobrod@meta.ua>
2018-03-30 18:34:07 +0300

    пример работы с константами в Sublime Text

---
+++ masiv2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>for Consts</title>
+</head>
+<body>
+	<?php
+define('WHAT_MAKE', 'Fucking my heart');
+define('WHERE_CITY', 'Kiev');
+define('WHEN_BACHELOUR', 'in 2014');
+define('WHEN_EXPERT', 'in 2018');
+
+$universityFirst = 'KPI';
+$universitySecond = 'NAU';
+
+?>
+</body>
+</html>
