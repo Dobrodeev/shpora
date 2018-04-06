67771c7a06bde8f14611b0b9f968b5e0ce0aea6b
Dobrodeev <dobrod@meta.ua>
2018-04-06 21:31:48 +0300

    Ïðîâåðèëè ÿâëÿåòñÿ ëè çàãðóæàåìûé ôàéë êàðòèíêîé, òî åñòü èìååò ðàçðåøåíèå .jpg, .png,... è åãî ãàáàðèòû Âûñîòà*Øèðèíà íå ïðåâûøàþò ìàêñèìàëüíûå

--- masiv2.php
+++ masiv2.php
@@ -134,27 +134,45 @@ if (isset($_POST['go']))
 	}
 }
 // сделать чтобы загружалась только картинка .jpg и ограничить размер картинки
-//$max_image_width = 1280;
-//$max_image_height =960;
-//$max_image_size = 960 * 1280;
-//$valid_types = array("gif","jpg", "png", "jpeg");
-//
-//if (isset($_FILES["userfile"])) {
-//if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-//    $filename = basename($_FILES['userfile']['name']);
-//    $ext = substr($_FILES['userfile']['name'], 1 + strrpos($_FILES['userfile']['name'], "."));
-//// strpos --  Возвращает позицию первого вхождения подстроки
-//
-//// получим массив свойств файла
-//    $size = GetImageSize($_FILES['userfile']['tmp_name']);
-//
-////проверим размер фото
-//    if (filesize($_FILES['userfile']['tmp_name']) > $max_image_size) {
-//        echo "Error: File size > " . $max_image_size;
-//    } elseif (!in_array($ext, $valid_types)) {
-//        echo 'Error: Invalid file type.';
-//    }
-//}
+$max_image_width = 1280;
+$max_image_height =960;
+$max_image_size = 960 * 1280;
+$valid_types = array("gif","jpg", "png", "jpeg");
+
+if (isset($_FILES["userfile"])) {
+if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    $filename = basename($_FILES['userfile']['name']);
+    $ext = substr($_FILES['userfile']['name'], 1 + strrpos($_FILES['userfile']['name'], "."));
+// strpos --  Возвращает позицию первого вхождения подстроки
+
+// получим массив свойств файла
+    $size = GetImageSize($_FILES['userfile']['tmp_name']);
+
+//проверим размер фото
+    if (filesize($_FILES['userfile']['tmp_name']) > $max_image_size) {
+        echo "Error: File size > " . $max_image_size;
+    } elseif (!in_array($ext, $valid_types)) {
+        echo 'Error: Invalid file type.';
+    }
+elseif(($size) && ($size[0] < $max_image_width) && ($size[1] < $max_image_height)){
+                    $uploaddir = 'uploads/';
+                    $uploadfile = $uploaddir.$filename;
+                    move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile);
+                    echo "<img src='$uploadfile' alt='$filename' title='$filename' />";
+                }
+                else{
+                    echo 'Error: invalid image properties.';
+                }
+
+        }
+        else { echo "Error: empty file.";}
+} else {
+echo '
+<form enctype="multipart/form-data" method="post">
+Send this file: <input name="userfile" type="file">
+<input type="submit" value="Send File">
+</form>';
+}
 ?>
 </body>
 </html>
