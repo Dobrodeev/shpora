ccbfc5cc04edcc1bfb8e7f7a06aff3bcce568868
dobrodeev <dobrod@meta.ua>
2018-06-21 18:16:30 +0300

    новый класс для выделения целой части у дроби

---
+++ BeautyFraction.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dobrodeev
+ * Date: 21.06.2018
+ * Time: 18:01
+ */
+
+class BeautyFraction extends Fraction
+{
+    /*
+    1
+  3 --
+    15
+    */
+    private $cel;
+
+
+}
